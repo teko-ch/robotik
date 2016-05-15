0020d4c18134432376999d4aec0adf7486c7b645
jbrutschin <j.brutschin@gmail.com>
2016-05-15 08:29:19 +0200

    TochSensor Listener Example

---
+++ Sensor/src/SensorListener.java
@@ -0,0 +1,39 @@
+import ch.aplu.ev3.*;
+
+public class SensorListener implements TouchListener {
+    private LegoRobot robot;
+    private TouchSensor sensor;
+
+    public SensorListener(){
+        robot = new LegoRobot();
+        sensor = new TouchSensor(SensorPort.S1);
+        sensor.addTouchListener(this);
+
+        robot.addPart(sensor);
+        robot.drawString("Press Sensor!", 0, 0);
+
+    }
+
+    private void run(){
+        while (!Tools.isEscapePressed()){
+
+        }
+        robot.exit();
+    }
+
+
+    public static void main(String[] args) {
+        SensorListener exp = new SensorListener();
+        exp.run();
+    }
+
+    @Override
+    public void pressed(SensorPort sensorPort) {
+        robot.drawString("pressed", 0, 4);
+    }
+
+    @Override
+    public void released(SensorPort sensorPort) {
+        robot.drawString("released", 0, 4);
+    }
+}
